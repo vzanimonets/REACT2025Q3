5c5a7ab0ecf65ee368156830c61f97d26e831adc
vzanimonets <vzanimonets@gmail.com>
2025-07-09 01:56:44 +0300

    feat: implementation of base ErrorBoundary component

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,25 @@
+import { Component } from 'react';
+import type { ReactNode } from 'react';
+type ErrorBoundaryProps = {
+  children: ReactNode;
+};
+type ErrorBoundaryState = {
+  hasError: boolean;
+};
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+  componentDidCatch(error: Error, info: React.ErrorInfo) {
+    console.error('ErrorBoundary caught an error', error, info);
+    this.setState({ hasError: true });
+  }
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong.</div>;
+    }
+    return this.props.children;
+  }
+}
+export default ErrorBoundary;
