2ece813790ab29ab2a2232414a450ad4a35641ff
vzanimonets <vzanimonets@gmail.com>
2025-07-09 16:46:51 +0300

    refactor: convert Card component to functional component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,25 @@
-import { Component } from 'react';
-class Card extends Component {
-  render() {
-    return <div>Card Component</div>;
-  }
+import type { SwapiPerson } from '../swapi';
+
+interface CardProps {
+  person: SwapiPerson;
 }
+
+function Card({ person }: CardProps) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        borderBottom: '1px solid #f0f0f0',
+        alignItems: 'center',
+        padding: '8px 0',
+      }}
+    >
+      <div style={{ flex: 2, paddingLeft: 8 }}>{person.name}</div>
+      <div style={{ flex: 3 }}>
+        Birth Year: {person.birth_year}, Gender: {person.gender}
+      </div>
+    </div>
+  );
+}
+
 export default Card;
