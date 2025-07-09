24a6ff976e27854a8d6d2ec9bb25bc42a73d306b
vzanimonets <vzanimonets@gmail.com>
2025-07-09 16:47:50 +0300

    refactor: update CardList to use functional Card component

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,7 +1,32 @@
-import { Component } from 'react';
-class CardList extends Component {
-  render() {
-    return <div>CardList Component</div>;
-  }
+import type { SwapiPerson } from '../swapi';
+import Card from './Card';
+
+interface CardListProps {
+  people: SwapiPerson[];
 }
+
+function CardList({ people }: CardListProps) {
+  if (!people.length) {
+    return <div>No results found.</div>;
+  }
+  return (
+    <div style={{ width: '100%' }}>
+      <div
+        style={{
+          display: 'flex',
+          fontWeight: 'bold',
+          borderBottom: '1px solid #ddd',
+          padding: '8px 0',
+        }}
+      >
+        <div style={{ flex: 2, paddingLeft: 8 }}>Name</div>
+        <div style={{ flex: 3 }}>Description</div>
+      </div>
+      {people.map((person) => (
+        <Card key={person.url} person={person} />
+      ))}
+    </div>
+  );
+}
+
 export default CardList;
