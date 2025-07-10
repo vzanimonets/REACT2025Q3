77604437cdb889376ac9b481c6eed68a6ed894ef
vzanimonets <vzanimonets@gmail.com>
2025-07-10 23:51:45 +0300

    refactor: make Search a controlled component, remove localStorage logic from Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,49 +1,27 @@
 import React, { type FormEvent, Component } from 'react';
 
 interface SearchProps {
-  onSearch: (query: string) => void;
+  value: string;
+  onChange: (value: string) => void;
+  onSearch: () => void;
 }
 
-interface SearchState {
-  hasValue: boolean;
-}
-
-class Search extends Component<SearchProps, SearchState> {
-  private inputRef = React.createRef<HTMLInputElement>();
-
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = { hasValue: false };
-  }
-
-  componentDidMount() {
-    const savedTerm = localStorage.getItem('searchTerm') || '';
-    if (this.inputRef.current) {
-      this.inputRef.current.value = savedTerm;
-      this.setState({ hasValue: !!savedTerm });
-    }
-  }
+class Search extends Component<SearchProps> {
+  handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.props.onChange(e.target.value);
+  };
 
   handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const query = this.inputRef.current?.value.trim() || '';
-    localStorage.setItem('searchTerm', query);
-    this.props.onSearch(query);
-  };
-
-  handleInput = () => {
-    this.setState({ hasValue: !!this.inputRef.current?.value });
+    this.props.onSearch();
   };
 
   handleClear = () => {
-    if (this.inputRef.current) {
-      this.inputRef.current.value = '';
-      this.inputRef.current.focus();
-      this.setState({ hasValue: false });
-    }
+    this.props.onChange('');
   };
 
   render() {
+    const { value } = this.props;
     return (
       <form
         className="flex items-center bg-white rounded-full shadow-md px-5 py-1 w-auto max-w-2xl mx-auto my-8 border border-gray-200"
@@ -53,12 +31,12 @@ class Search extends Component<SearchProps, SearchState> {
         <input
           type="text"
           placeholder="Search by name..."
-          ref={this.inputRef}
+          value={value}
+          onChange={this.handleInput}
           className="flex-1 min-w-[300px] text-xl py-3 bg-transparent border-none outline-none shadow-none focus:outline-none"
           aria-label="Search input"
-          onInput={this.handleInput}
         />
-        {this.state.hasValue && (
+        {value && (
           <button
             type="button"
             className="bg-transparent border-none outline-none cursor-pointer px-2 flex items-center h-10 mr-1 focus:outline-none"
