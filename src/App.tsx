c14ebd119fb44a6332bb33812d08f006a20b782d
vzanimonets <vzanimonets@gmail.com>
2025-07-11 16:32:21 +0300

    refactor: use SearchContainer and ResultsContainer in App layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,8 @@ import SearchContainer from './components/SearchContainer';
 import ResultsContainer from './components/ResultsContainer';
 import ErrorBoundary from './components/ErrorBoundary';
 import ErrorButton from './components/ErrorButton';
-import { fetchPeople } from './api/swapi';
-import type { SwapiPerson } from './api/swapi';
 import type { ReactNode } from 'react';
 import { Component } from 'react';
-
-interface ErrorObject {
-  text: string;
-  errorCode?: number;
-}
-
-interface AppState {
-  searchTerm: string;
-  people: SwapiPerson[];
-  loading: boolean;
-  error: ErrorObject | null;
-  artificialError: Error | null;
-}
 
 class App extends Component<object> {
   render(): ReactNode {
@@ -36,7 +21,11 @@ class App extends Component<object> {
               <ResultsContainer boundaryError={boundaryError} />
             </main>
             <div className="flex justify-end mt-4">
-              <ErrorButton onThrowError={() => { throw new Error('Test error from ErrorButton'); }} />
+              <ErrorButton
+                onThrowError={() => {
+                  throw new Error('Test error from ErrorButton');
+                }}
+              />
             </div>
           </div>
         )}
