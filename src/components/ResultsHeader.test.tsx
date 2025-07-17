4a094a21297bf71c58540d875b30940b762a4a42
vzanimonets <vzanimonets@gmail.com>
2025-07-17 16:02:02 +0300

    test: update  ResultsHeader component tests

--- src/components/ResultsHeader.test.tsx
+++ src/components/ResultsHeader.test.tsx
@@ -1,8 +1,19 @@
-import { render } from '@testing-library/react';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import ResultsHeader from './ResultsHeader';
 
 describe('ResultsHeader', () => {
-  it('should render without crashing', () => {
+  it('renders all column headers', () => {
     render(<ResultsHeader />);
+    expect(screen.getByText('Name')).toBeInTheDocument();
+    expect(screen.getByText('Height')).toBeInTheDocument();
+    expect(screen.getByText('Mass')).toBeInTheDocument();
+    expect(screen.getByText('Hair Color')).toBeInTheDocument();
+    expect(screen.getByText('Skin Color')).toBeInTheDocument();
+    expect(screen.getByText('Eye Color')).toBeInTheDocument();
+    expect(screen.getByText('Birth Year')).toBeInTheDocument();
+    expect(screen.getByText('Gender')).toBeInTheDocument();
+    expect(screen.getByText('Films')).toBeInTheDocument();
+    expect(screen.getByText('Vehicles+Starships')).toBeInTheDocument();
   });
 });
