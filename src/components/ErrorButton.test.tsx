88170d731cdb2a4893a0903ff4464572f2d29b58
vzanimonets <vzanimonets@gmail.com>
2025-07-25 17:33:11 +0300

    refactor: add expect to the ErrorButton.test.tsx

--- src/components/ErrorButton.test.tsx
+++ src/components/ErrorButton.test.tsx
@@ -3,6 +3,7 @@ import ErrorButton from './ErrorButton';
 
 describe('ErrorButton', () => {
   it('should render without crashing', () => {
-    render(<ErrorButton onThrowError={() => {}} />);
+    const { getByRole } = render(<ErrorButton onThrowError={() => {}} />);
+    expect(getByRole('button')).toBeInTheDocument();
   });
 });
