7eeab685cc2fd024a9c82e29182b84b9060c38f4
vzanimonets <vzanimonets@gmail.com>
2025-07-25 17:43:45 +0300

    refactor: add case where use props

--- src/components/Results.test.tsx
+++ src/components/Results.test.tsx
@@ -15,4 +15,37 @@ describe('Results', () => {
       />
     );
   });
+
+  it('renders person name when people prop is provided', () => {
+    const people = [
+      {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        hair_color: 'blond',
+        skin_color: 'fair',
+        eye_color: 'blue',
+        birth_year: '19BBY',
+        gender: 'male',
+        homeworld: 'Tatooine',
+        films: [],
+        species: [],
+        vehicles: [],
+        starships: [],
+        created: '',
+        edited: '',
+        url: '',
+      },
+    ];
+    const { getByText } = render(
+      <Results
+        people={people}
+        loading={false}
+        error={null}
+        searchTerm="Luke"
+        highlight={false}
+      />
+    );
+    expect(getByText('Luke Skywalker')).toBeInTheDocument();
+  });
 });
